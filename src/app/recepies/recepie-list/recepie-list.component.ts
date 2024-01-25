60ff133cb9c9b8e96ba006c9bf79f981ef2e097b
krishnamohanparuchuri <paruchurikris@gmail.com>
2024-01-25 17:45:15 +0100

    added the relavent components

---
+++ src/app/recepies/recepie-list/recepie-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recepie-list',
+  templateUrl: './recepie-list.component.html',
+  styleUrl: './recepie-list.component.css'
+})
+export class RecepieListComponent {
+
+}
